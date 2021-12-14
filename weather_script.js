e46e2d850735a2f0c87a6b16ce8ed5dd104cf148
Vardhan Rajnikant Shah <vardhan.s@ahduni.edu.in>
2021-12-14 09:10:40 +0530

    displaying values on front screen by making a object.

--- weather_script.js
+++ weather_script.js
@@ -1,35 +1,49 @@
-let day = document.getElementById("day-display");
-let date = document.getElementById("date-display");
-let loc = document.getElementById("location-display");
 let apiurl = "https://api.openweathermap.org/data/2.5/weather";
 let apiKey = "8c5fb5f3eb2951506bf866a3f972385b";
-day.innerHTML = "Friday";
-console.log(day.innerHTML);
 
+const values = {
+    day : document.getElementById("day-display"),
+    date: document.getElementById("date-display"),
+    city : document.getElementById("location-display"),
+    temprature : document.getElementById("weather-temperature"),
+    precipitation : document.getElementById("prec-value"),
+    humidity : document.getElementById("humid-value"),
+    wind : document.getElementById("wind-value"),
+    desc : document.getElementById("weather-type")
+}
 
 function coordinate_success(position){
 
     var coordinate = position.coords;
     var latitude = coordinate.latitude;
     var longitude = coordinate.longitude;
-
-    console.log("Latitude : " + latitude);
-    console.log("Longitude : " + longitude);
-    get_weather_by_coordinates(latitude,longitude);
+    get_weather_by_coordinates(latitude,longitude,display_values);
 
 }
-async function get_weather_by_coordinates(lat,lon){
+
+async function get_weather_by_coordinates(lat,lon,jsoncallback){
     let url = apiurl+"?lat="+lat+"&lon="+lon+"&appid="+apiKey;
-    console.log(url);
-    let response = await fetch(url);
-    let json =  await response.json();
-    console.log(json);
+    json = await load_json(url);
+    //console.log(json);
+    jsoncallback(json);
 }
 
+async function load_json(str1){
+    let promise = await fetch(str1);
+    let response =  promise.json();
+    return response;
+}
 
+function display_values(json){
+    console.log(json);
+    values.city.innerHTML = json["name"];
+    values.temprature.innerHTML = json["main"]["temp"] + " C";
+    values.desc.innerHTML = json["weather"][0]["main"];
+    values.precipitation.innerHTML = json["clouds"]["all"] +" %";
+    values.humidity.innerHTML = json["main"]["humidity"] + " %";
+    values.wind.textContent = json["wind"]["speed"] + " km/h";
+    //values.date.innerHTML = (new Date()).toString();
+}
 
 navigator.geolocation.getCurrentPosition(coordinate_success);
-get_weather_by_coordinates(21.170240,72.831062);
-//let fetchRes = fetch("https://jsonplaceholder.typicode.com/todos/1");
-
-//fetchRes.then(response => response.json()).then(data => console.log(data));
+//get_weather_by_coordinates(21.170240,72.831062,display_values);
